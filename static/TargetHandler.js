5284a016cd5ce788d1fd38c828277a846384c17d
Alex Johnson <alexjo5000@gmail.com>
2018-02-19 18:27:44 -0500

    Finished preview shapes

--- static/TargetHandler.js
+++ static/TargetHandler.js
@@ -63,12 +63,24 @@ var TargetsHandler  = function (id, targets, response) {
     shapeSelector.ctx.save();
     shapeSelector.ctx.clearRect(0, canvas.height/2, canvas.width, canvas.height/2);
     shapeSelector.ctx.fillStyle = newTargetBuffer.shape_color || "#000000";
+    var charOffset = { x: 0, y: 0 };
     switch (newTargetBuffer.shape) {
       case "circle":
+        shapeSelector.ctx.beginPath();
+        shapeSelector.ctx.arc(100, 300, 65, 0, 2 * Math.PI);
+        shapeSelector.ctx.fill();
         break;
       case "semicircle":
+        shapeSelector.ctx.beginPath();
+        shapeSelector.ctx.arc(100, 260, 80, 0, Math.PI);
+        shapeSelector.ctx.fill();
         break;
       case "quarter_circle":
+        shapeSelector.ctx.beginPath();
+        shapeSelector.ctx.arc(40, 240, 120, 0, Math.PI / 2);
+        shapeSelector.ctx.lineTo(40, 240);
+        shapeSelector.ctx.fill();
+        charOffset = { x: -10, y: -10 };
         break;
       case "square":
         shapeSelector.ctx.fillRect(50, 250, 100, 100);
@@ -77,14 +89,28 @@ var TargetsHandler  = function (id, targets, response) {
         shapeSelector.ctx.fillRect(30, 260, 140, 80);
         break;
       case "trapezoid":
+        shapeSelector.ctx.beginPath();
+        shapeSelector.ctx.moveTo( 60, 260);
+        shapeSelector.ctx.lineTo( 20, 340);
+        shapeSelector.ctx.lineTo(180, 340);
+        shapeSelector.ctx.lineTo(140, 260);
+        shapeSelector.ctx.fill();
+        break;
+      case "triangle":
+        shapeSelector.ctx.beginPath();
+        shapeSelector.ctx.moveTo(100, 240);
+        shapeSelector.ctx.lineTo(170, 360);
+        shapeSelector.ctx.lineTo( 30, 360);
+        shapeSelector.ctx.fill();
+        charOffset = { x: 0, y: 10 };
         break;
       case "pentagon":
         shapeSelector.ctx.beginPath();
         shapeSelector.ctx.moveTo(100, 230);
-        shapeSelector.ctx.lineTo( 30, 280);
-        shapeSelector.ctx.lineTo( 60, 350);
-        shapeSelector.ctx.lineTo(140, 350);
-        shapeSelector.ctx.lineTo(170, 280);
+        shapeSelector.ctx.lineTo( 32, 287);
+        shapeSelector.ctx.lineTo( 60, 360);
+        shapeSelector.ctx.lineTo(140, 360);
+        shapeSelector.ctx.lineTo(168, 287);
         shapeSelector.ctx.fill();
         break;
       case "hexagon":
@@ -110,6 +136,19 @@ var TargetsHandler  = function (id, targets, response) {
         shapeSelector.ctx.fill();
         break;
       case "star":
+        /* TODO: make me pretty :( */
+        shapeSelector.ctx.beginPath();
+        shapeSelector.ctx.moveTo(100, 225);
+        shapeSelector.ctx.lineTo(125, 260);
+        shapeSelector.ctx.lineTo(175, 260);
+        shapeSelector.ctx.lineTo(150, 300);
+        shapeSelector.ctx.lineTo(165, 350);
+        shapeSelector.ctx.lineTo(100, 330);
+        shapeSelector.ctx.lineTo( 35, 350);
+        shapeSelector.ctx.lineTo( 50, 300);
+        shapeSelector.ctx.lineTo( 25, 260);
+        shapeSelector.ctx.lineTo( 75, 260);
+        shapeSelector.ctx.fill();
         break;
       case "cross":
         shapeSelector.ctx.fillRect(80, 240, 40, 120);
@@ -118,7 +157,8 @@ var TargetsHandler  = function (id, targets, response) {
     }
     shapeSelector.ctx.fillStyle = newTargetBuffer.alphanumeric_color || "#000000";
     shapeSelector.ctx.font="60px monospace";
-    shapeSelector.ctx.fillText(newTargetBuffer.alphanumeric, 80, 320);
+    shapeSelector.ctx.fillText(newTargetBuffer.alphanumeric,
+      80 + charOffset.x, 320 + charOffset.y);
     shapeSelector.ctx.restore();
   }
 
