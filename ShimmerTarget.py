fd75a447da596824eb039edf469db1a3d6d25f81
TheNexusEclipse <tyrrellufot99@gmail.com>
2018-04-25 19:57:23 -0400

    Update ShimmerTarget.py

--- ShimmerTarget.py
+++ ShimmerTarget.py
@@ -5,12 +5,9 @@ class ShimmerTarget(object):
     def __init__(self, target_region):
         self.target_region = target_region
         self.valid = True
-        print(self.target_region)
         tgtimage = self.target_region.image.jpg() 
         tgtimage = cv2.imread(tgtimage)
         crop_img = tgtimage[self.target_region.coord1[1]:self.target_region.coord2[1], self.target_region.coord1[0]:self.target_region.coord2[0]] #Set coordinates for cropped image
-        print("BLANK TEXTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT")
-        print(str(target_region.target_region_id))
         cv2.imwrite(self.target_region.flight.folder + "/targets/target_" + str(target_region.target_region_id) + ".jpg", crop_img)#Save the cropped image
 
     @property
