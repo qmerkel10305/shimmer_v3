64f89c3d7e77f9a960958491541e6e2427a775d0
Colin Moore <colin@moore.one>
2018-10-09 15:13:10 -0400

    Add error message on failed image load

--- frontend/src/app/images/images.component.ts
+++ frontend/src/app/images/images.component.ts
@@ -25,12 +25,18 @@ export class ImagesComponent implements AfterViewInit {
   update() {
     let height = this.canvas.nativeElement.height = window.innerHeight;
     let width = this.canvas.nativeElement.width = window.innerWidth;
-    this.service.getNext().subscribe((data: any) => {
-        this.image = new (window as any).Image();
-        this.image.src = this.service.getImageURL(data["data"]);
-        this.image.onload = () => {
-            this.context.drawImage(this.image, 0, 0, width, height);
+    this.service.getNext().subscribe(
+        (data: any) => {
+            this.image = new (window as any).Image();
+            this.image.src = this.service.getImageURL(data["data"]);
+            this.image.onload = () => {
+                this.context.drawImage(this.image, 0, 0, width, height);
+            }
+        },
+        (error: any) => {
+            alert("Failed to load next image: " + error.message);
+            console.log(error);
         }
-    });
+    );
   }
 }
