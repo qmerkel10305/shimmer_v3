5e4a02bf33aab4d607f1272208b1f007f2a8b2cc
Colin Moore <camoore7@ncsu.edu>
2019-06-06 14:41:02 -0400

    Ignore orientation in images when creating thumbnails
    Change-Id: Idc30b1c8518ca12d52439e020ee2e5b6b0cf25c0

--- server/models/ShimmerTargetRegion.py
+++ server/models/ShimmerTargetRegion.py
@@ -2,6 +2,7 @@ import cv2
 
 from server.util.JSONObject import JSONObject
 
+
 class ShimmerTargetRegion(JSONObject):
     def __init__(self, target_region):
         self.target_region = target_region
@@ -12,12 +13,14 @@ class ShimmerTargetRegion(JSONObject):
 
     def create_thumbnail(self):
         tgtimage = self.target_region.image.jpg()
-        tgtimage = cv2.imread(tgtimage)
+
+        tgtimage = cv2.imread(tgtimage, cv2.IMREAD_IGNORE_ORIENTATION)
         # Set coordinates for cropped image
         crop_img = tgtimage[int(self.target_region.coord1[1]):int(self.target_region.coord2[1]),
                             int(self.target_region.coord1[0]):int(self.target_region.coord2[0])]
         # Save the cropped image
-        thumbnail_path = self.target_region.flight.folder + "/targets/target_" + str(self.target_region.target_region_id) + ".jpg"
+        thumbnail_path = self.target_region.flight.folder + "/targets/target_" + \
+            str(self.target_region.target_region_id) + ".jpg"
         cv2.imwrite(thumbnail_path, crop_img)
         self.target_region.target.update_thumbnail(thumbnail_path)
 
@@ -30,8 +33,8 @@ class ShimmerTargetRegion(JSONObject):
             'id': self.id,
             'target_id': self.target_region.target.target_id,
             'image_id': self.target_region.image.image_id,
-            'a': {'x':self.target_region.coord1[0], 'y':self.target_region.coord1[1]},
-            'b': {'x':self.target_region.coord2[0], 'y':self.target_region.coord2[1]},
+            'a': {'x': self.target_region.coord1[0], 'y': self.target_region.coord1[1]},
+            'b': {'x': self.target_region.coord2[0], 'y': self.target_region.coord2[1]},
         }
 
     def deserialize(self, json_data):
@@ -45,4 +48,5 @@ class ShimmerTargetRegion(JSONObject):
         if new_target['image_id'] != self.target_region.image.image_id:
             raise ValueError("Target Region Image ID does not match")
 
-        raise NotImplementedError("Deserializing and editing target region values is not supported")
+        raise NotImplementedError(
+            "Deserializing and editing target region values is not supported")
