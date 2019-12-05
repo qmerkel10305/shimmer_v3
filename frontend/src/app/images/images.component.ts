93d05e845fea0a4e4d291e2b0154358d3feca550
Brooke Raschke <bmraschk@ncsu.edu>
2019-12-04 20:08:28 -0500

    Fixed target selection with correct aspect ratio
    Grabs selection at the correct location
    Doesn't resize canvas
    Fixed edge cases where selection is outside of image

--- frontend/src/app/images/images.component.ts
+++ frontend/src/app/images/images.component.ts
@@ -30,6 +30,8 @@ export class ImagesComponent implements AfterViewInit {
   private xDifference: number;
   private yDifference: number;
   private canvasElement: HTMLCanvasElement;
+  private imageHeight: number;
+  private imageWidth: number;
 
   @ViewChild('shimmerCanvas') private canvas: ElementRef;
   @ViewChild(TargetClassifierComponent) private classifierWindow: TargetClassifierComponent;
@@ -47,19 +49,19 @@ export class ImagesComponent implements AfterViewInit {
 
   update() {
     this.service.getNext().subscribe(
-        (image: Image) => {
-            this.image = image;
-            this.imageString = 'Image Id: ' + image.id.toString();
-            this.imageElement = new (window as any).Image();
-            this.imageElement.src = this.service.getImageURL(image);
-            this.imageElement.onload = () => {
-                this.render();
-            };
-        },
-        (error: any) => {
-            alert('Failed to load next image: ' + error.message);
-            console.error(error);
-        }
+      (image: Image) => {
+        this.image = image;
+        this.imageString = 'Image Id: ' + image.id.toString();
+        this.imageElement = new (window as any).Image();
+        this.imageElement.src = this.service.getImageURL(image);
+        this.imageElement.onload = () => {
+          this.render();
+        };
+      },
+      (error: any) => {
+        alert('Failed to load next image: ' + error.message);
+        console.error(error);
+      }
     );
   }
 
@@ -67,9 +69,9 @@ export class ImagesComponent implements AfterViewInit {
    * Uses the service getImage to subscribe to the selected image
    * @param id the id of the image to get
    */
-  getImage(id){
+  getImage(id) {
     id = Math.round(id);
-    if(!isNaN(id) && id >= 0) {
+    if (!isNaN(id) && id >= 0) {
       this.service.getImage(id).subscribe(
         (image: Image) => {
           this.image = image;
@@ -82,7 +84,7 @@ export class ImagesComponent implements AfterViewInit {
         },
         (error: any) => {
           alert('Failed to load next image: ' + error.message);
-            console.error(error);
+          console.error(error);
         }
       );
     }
@@ -92,40 +94,38 @@ export class ImagesComponent implements AfterViewInit {
    * Render the image and all target regions
    */
   private render() {
-    let height = this.canvas.nativeElement.height = window.innerHeight;
-    let width = this.canvas.nativeElement.width = window.innerWidth;
-    this.context.clearRect(0, 0, width, height);
-    console.log(this.imageElement.width + " " + this.imageElement.height);
+    this.imageHeight = this.canvasElement.height = window.innerHeight;
+    this.imageWidth = this.canvasElement.width = window.innerWidth;
+
+    this.context.clearRect(0, 0, this.imageWidth, this.imageHeight);
+
     const imageRatio = this.imageElement.width / this.imageElement.height;
-    console.log(imageRatio);
-    const windowRatio = width / height;
+    const windowRatio = this.imageWidth / this.imageHeight;
 
     if (imageRatio < windowRatio) {
-      height = window.innerHeight;
-      width = window.innerHeight * imageRatio;
+      this.imageWidth = window.innerHeight * imageRatio;
     } else {
-      height = window.innerWidth / imageRatio;
-      width = window.innerWidth;
+      this.imageHeight = window.innerWidth / imageRatio;
     }
 
-    this.xDifference = window.innerWidth - width;
-    this.yDifference = window.innerHeight - height;
+    this.xDifference = (window.innerWidth - this.imageWidth) / 2;
+    this.yDifference = (window.innerHeight - this.imageHeight) / 2;
 
-    this.context.drawImage(this.imageElement, this.xDifference / 2, this.yDifference / 2, width, height);
+    this.context.drawImage(this.imageElement, this.xDifference, this.yDifference, this.imageWidth, this.imageHeight);
     this.image.targets.forEach((targetRegion: TargetRegion) => {
-        this.renderTargetRegion(targetRegion);
+      this.renderTargetRegion(targetRegion);
     });
-    if ( this.selection != null && this.selection.b != null) {
-        this.renderTargetRegion(this.selection);
+    if (this.selection != null && this.selection.b != null) {
+      this.renderTargetRegion(this.selection);
     }
   }
 
   private renderTargetRegion(targetRegion: TargetRegion) {
     this.context.fillStyle = 'rgba(127, 255, 127, 0.3)';
-    const x1 = targetRegion.a.x * (this.canvas.nativeElement.width / this.imageElement.width);
-    const y1 = targetRegion.a.y * (this.canvas.nativeElement.height / this.imageElement.height);
-    const x2 = targetRegion.b.x * (this.canvas.nativeElement.width / this.imageElement.width);
-    const y2 = targetRegion.b.y * (this.canvas.nativeElement.height / this.imageElement.height);
+    const x1 = (targetRegion.a.x * (this.imageWidth / this.imageElement.width)) + this.xDifference;
+    const y1 = (targetRegion.a.y * (this.imageHeight / this.imageElement.height)) + this.yDifference;
+    const x2 = (targetRegion.b.x * (this.imageWidth / this.imageElement.width)) + this.xDifference;
+    const y2 = (targetRegion.b.y * (this.imageHeight / this.imageElement.height)) + this.yDifference;
     this.context.fillRect(x1, y1, x2 - x1, y2 - y1);
   }
 
@@ -139,15 +139,15 @@ export class ImagesComponent implements AfterViewInit {
 
   /****************************** Target Classifier Handlers ******************************/
 
-  targetSubmitted(_: Target) {}
+  targetSubmitted(_: Target) { }
 
   targetRegionSubmitted(_: TargetRegion) {
     // Ensure this image is up to date
     this.service.getImage(this.image.id).subscribe((image: Image) => {
-        this.image = image;
-        this.render();
+      this.image = image;
+      this.render();
     }, (error) => {
-        console.error(error);
+      console.error(error);
     });
   }
 
@@ -155,52 +155,77 @@ export class ImagesComponent implements AfterViewInit {
     this.service.getImage(this.image.id).subscribe((image: Image) => {
       this.image = image;
       this.render();
-  }, (error) => {
+    }, (error) => {
       console.error(error);
-  });
+    });
   }
 
   /********************************* Mouse Event Handlers *********************************/
 
   mouseDown(event) {
-    if ( this.locked) {
-        // Exit if the target classifier window is showing
-        return;
+    if (this.locked) {
+      // Exit if the target classifier window is showing
+      return;
     }
-    const point = new Point(event.x * (this.imageElement.width / this.canvas.nativeElement.width),
-                          event.y * (this.imageElement.height / this.canvas.nativeElement.height));
+    if(event.x < this.xDifference || event.y < this.yDifference || 
+      event.x > this.xDifference + this.imageWidth || event.y > this.yDifference + this.imageHeight) {
+      return;
+    }
+    // translate coordinates from window to canvas to scaled picture
+    const point = new Point(Math.round((event.x - this.xDifference) * (this.imageElement.width / this.imageWidth)),
+      Math.round((event.y - this.yDifference) * (this.imageElement.height / this.imageHeight)));
+    console.log(JSON.stringify(point));
     this.image.targets.forEach((targetRegion: TargetRegion) => {
-        if ( targetRegion.contains(point)) {
-            this.lock();
-            this.service.getTarget(targetRegion.target_id).subscribe((target: Target) => {
-                this.classifierWindow.show(this.imageElement, targetRegion, this.image, target);
-            },
-            (error) => {
-                console.error(error);
-                this.unlock();
-            });
-            return;
-        }
+      if (targetRegion.contains(point)) {
+        this.lock();
+        this.service.getTarget(targetRegion.target_id).subscribe((target: Target) => {
+          this.classifierWindow.show(this.imageElement, targetRegion, this.image, target);
+        },
+          (error) => {
+            console.error(error);
+            this.unlock();
+          });
+        return;
+      }
     });
     if (this.locked) {
-        // Exit if the target classifier window is showing
-        return;
+      // Exit if the target classifier window is showing
+      return;
     }
     this.selecting = true;
     this.selection = new TargetRegion(new Point(
-            Math.round(event.x * (this.imageElement.width / this.canvas.nativeElement.width)),
-            Math.round(event.y * (this.imageElement.height / this.canvas.nativeElement.height))
-        ), null, null, this.image.id);
+      Math.round((event.x - this.xDifference) * (this.imageElement.width / this.imageWidth)),
+      Math.round((event.y - this.yDifference) * (this.imageElement.height / this.imageHeight))
+    ), null, null, this.image.id);
   }
 
   mouseMove(event) {
     if (!this.selecting) {
-        // Exit if an area is not currently being selected
-        return;
+      // Exit if an area is not currently being selected
+      return;
     }
+
+    let x, y;
+
+    if (event.x > this.imageWidth + this.xDifference) {
+      x = this.imageWidth + this.xDifference;
+    } else if (event.x < this.xDifference) {
+      x = this.xDifference;
+    } else {
+      x = event.x;
+    }
+
+    if (event.y > this.imageHeight + this.yDifference) {
+      y = this.imageHeight + this.yDifference;
+    } else if ( event.y < this.yDifference) {
+      y = this.yDifference;
+    } else {
+      y = event.y;
+    }
+
     this.selection.b = new Point(
-        Math.round(event.x * (this.imageElement.width / this.canvas.nativeElement.width)),
-        Math.round(event.y * (this.imageElement.height / this.canvas.nativeElement.height))
+      Math.round((x - this.xDifference) * (this.imageElement.width / this.imageWidth)),
+      Math.round((y - this.yDifference) * (this.imageElement.height / this.imageHeight))
     );
 
     this.render();
@@ -208,23 +233,46 @@ export class ImagesComponent implements AfterViewInit {
 
   mouseUp(event) {
     if (this.locked) {
-        // Exit if the target classifier window is showing
-        return;
+      // Exit if the target classifier window is showing
+      return;
     }
+    if (!this.selecting) {
+      return;
+    }
+
+    let x, y;
+
+    if (event.x > this.imageWidth + this.xDifference) {
+      x = this.imageWidth + this.xDifference;
+    } else if (event.x < this.xDifference) {
+      x = this.xDifference;
+    } else {
+      x = event.x;
+    }
+
+    if (event.y > this.imageHeight + this.yDifference) {
+      y = this.imageHeight + this.yDifference;
+    } else if ( event.y < this.yDifference) {
+      y = this.yDifference;
+    } else {
+      y = event.y;
+    }
+
     this.selecting = false;
     this.selection.b = new Point(
-        Math.round(event.x * (this.imageElement.width / this.canvas.nativeElement.width)),
-        Math.round(event.y * (this.imageElement.height / this.canvas.nativeElement.height))
+      Math.round((x - this.xDifference) * (this.imageElement.width / this.imageWidth)),
+      Math.round((y - this.yDifference) * (this.imageElement.height / this.imageHeight))
     );
+
     if (this.selection.a.delta(this.selection.b) < ImagesComponent.MIN_SELECTION_SIZE) {
-        this.selection = null;
-        return;
+      this.selection = null;
+      return;
     }
     if (this.selection.a.x > this.selection.b.x) {
-        // This ensures a is smaller than b
-        const temp = this.selection.a;
-        this.selection.a = this.selection.b;
-        this.selection.b = temp;
+      // This ensures a is smaller than b
+      const temp = this.selection.a;
+      this.selection.a = this.selection.b;
+      this.selection.b = temp;
     }
 
     this.lock();
@@ -235,24 +283,24 @@ export class ImagesComponent implements AfterViewInit {
   }
 
   @HostListener('document:keydown', ['$event'])
-  keyDown(event: KeyboardEvent){
-    if(this.locked) {
+  keyDown(event: KeyboardEvent) {
+    if (this.locked) {
       return;
     }
-    switch(event.keyCode){
-        case 13: //Enter Key
-            this.update();
-            break;
-        case 37: //Left Arrow Key
-            this.getImage(this.image.id - 1);
-            break;
-        case 38: //Up Arrow Key
-            var id = parseInt(prompt("Image id number?", "0"));
-            this.getImage(id);
-            break;
-        case 39: //Right Arrow Key
-            this.getImage(this.image.id + 1);
-            break;
+    switch (event.keyCode) {
+      case 13: // Enter Key
+        this.update();
+        break;
+      case 37: // Left Arrow Key
+        this.getImage(this.image.id - 1);
+        break;
+      case 38: // Up Arrow Key
+        var id = parseInt(prompt("Image id number?", "0"));
+        this.getImage(id);
+        break;
+      case 39: // Right Arrow Key
+        this.getImage(this.image.id + 1);
+        break;
     }
   }
 }
