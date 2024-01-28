4bc4fa2e7b137766c91c7be18e5bd712aae05370
Mach1212 <maciej.pruchnik@gmail.com>
2024-01-27 22:34:23 -0500

    [Feat] Finish login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,26 @@
+import { Autocomplete, TextField, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  return (
+    <main className="flex flex-col items-center w-screen h-screen justify-center">
+      <Typography variant="h3" className="italic">
+        Shimmer
+      </Typography>
+      <Autocomplete
+        disablePortal
+        id="combo-box-demo"
+        options={["something", "another"]}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField {...params} label="Flight Id" />}
+        onChange={(event) => {
+          let flightId = event.target.textContent;
+          console.log(flightId);
+          navigate("/shimmer", { state: { flightId } });
+        }}
+      />
+    </main>
+  );
+}
