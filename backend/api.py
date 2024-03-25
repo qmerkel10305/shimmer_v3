3db681ac33a3a7f3c3f12308210b612e9325dd50
qmerkel0305 <mcqm105@gmail.com>
2024-03-25 10:07:55 -0400

    Added some documentation

--- backend/api.py
+++ backend/api.py
@@ -1,5 +1,14 @@
 from PIL import Image
-from fastapi import FastAPI, UploadFile, File, Form, WebSocket, HTTPException, Response, WebSocketDisconnect
+from fastapi import (
+    FastAPI,
+    UploadFile,
+    File,
+    Form,
+    WebSocket,
+    HTTPException,
+    Response,
+    WebSocketDisconnect,
+)
 import io
 import os
 from typing import Optional
@@ -20,7 +29,7 @@ global firstSend
 firstSend = True
 
 
-#Declare FastAPI App
+# Declare FastAPI App
 app = FastAPI()
 
 # Sets up CORS Permissions
@@ -35,77 +44,83 @@ app.add_middleware(
 
 # Create the connection manager (this is for one client, different manager for multiple)
 class Manager:
-    '''
+    """
     Class to manage the websocket connection
-    '''
+    """
 
     def __init__(self) -> None:
-        self.noConnectionException = HTTPException(status_code=400,detail="No connection active")
+        self.noConnectionException = HTTPException(
+            status_code=400, detail="No connection active"
+        )
         self.active_connection: WebSocket = None
 
     async def connect(self, websocket: WebSocket) -> None:
-        '''
+        """
         Sets the Manager's active connection to the Websocket connection
-        '''
+        """
         self.active_connection = websocket
 
     async def disconnect(self, websocket: WebSocket) -> None:
-        '''
+        """
         Closes the WS connection, and removes the active connection
-        '''
+        """
         await self.active_connection.close()
         self.active_connection = None
-        
-    async def send_str(self, data:str | int) -> None:
-        '''
+
+    async def send_str(self, data: str | int) -> None:
+        """
         Sends a str over the WS connection
-        '''
+        """
         if self.active_connection == None:
             raise self.noConnectionException
         await self.active_connection.send_text(data)
-        
+
     async def sendImgData(self, flight_id: str | int, img_id: str | int) -> None:
-        '''
+        """
         Sends an img's data over the WS connection
-        '''
+        """
         if self.active_connection == None:
             raise self.noConnectionException
-        data = {"type":"img","flight_id":flight_id,"img_id":img_id}
+        data = {"type": "img", "flight_id": flight_id, "img_id": img_id}
 
         await self.active_connection.send_json(data)
-    
 
-#Declare Minio Server
+
+# Declare Minio Server
 client = Minio(
     "db:" + os.environ.get("DB_API_EXTERNAL_PORT", "9000"),
     access_key=os.environ.get("MINIO_ROOT_USER", "admin"),
     secret_key=os.environ.get("MINIO_ROOT_PASSWORD", "arcshimmer"),
-    secure=False
+    secure=False,
 )
-temp_directory="./temp_images"
+temp_directory = "./temp_images"
 global activeFlight
 activeFlight = os.environ.get("FLIGHT_ID", "testimages")
 
 global watchingFlight
 watchingFlight = ""
 
-adlcAddress = "0.0.0.0:{0}".format(os.environ.get("ADLC_PORT",None))
+adlcAddress = "0.0.0.0:{0}".format(os.environ.get("ADLC_PORT", None))
+
 
 def getLatestBucket():
-    '''
+    """
     Returns the most recent bucket's name
-    '''
-    return sorted(client.list_buckets(), key=lambda bucket: bucket.creation_date, reverse=True)[0].name
-
+    """
+    return sorted(
+        client.list_buckets(), key=lambda bucket: bucket.creation_date, reverse=True
+    )[0].name
 
 
 manager = Manager()
+
+
 @app.websocket("/ws")
-async def websocket(websocket:WebSocket):
-    '''
+async def websocket(websocket: WebSocket):
+    """
     Websocket endpoint for the frontend to view images.\n
     The frontend will send an empty string or None if the user is looking for a live feed, and the name of a bucket if they are wanting to look at a past flight
-    '''
+    """
     global activeFlight
     global watchingFlight
     global firstSend
@@ -114,64 +129,67 @@ async def websocket(websocket:WebSocket):
     try:
         while True:
             print("truly waiting")
-            data =  await manager.active_connection.receive_json()
+            data = await manager.active_connection.receive_json()
             print(data)
-            if  isinstance(data,dict):
-                if 'type' in data:
-                    type = data['type']
-                if 'flight_id' in data:
-                    if data['flight_id'] == "" or data["flight_id"] == None:
-                        #Control flow goes here if the user wants a live feed
+            if isinstance(data, dict):
+                if "type" in data:
+                    type = data["type"]
+                if "flight_id" in data:
+                    if data["flight_id"] == "" or data["flight_id"] == None:
+                        # Control flow goes here if the user wants a live feed
                         try:
-                            #Checks if the first image downlink of the flight has occured, if not it creates a new bucket for the flight, and tells the rest of the backend that it doesn't need to create a new bucket
+                            # Checks if the first image downlink of the flight has occured, if not it creates a new bucket for the flight, and tells the rest of the backend that it doesn't need to create a new bucket
                             if firstSend == True:
-                                activeFlight = str(datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S"))
+                                activeFlight = str(
+                                    datetime.datetime.now().strftime(
+                                        "%Y.%m.%d-%H.%M.%S"
+                                    )
+                                )
                             else:
                                 activeFlight = getLatestBucket()
                             firstSend = False
                             checkBucket()
-                            #Because the user is wanting a live feed, sets the flight that they are watching to the currently active flight
+                            # Because the user is wanting a live feed, sets the flight that they are watching to the currently active flight
                             watchingFlight = getLatestBucket()
-                            #Sends all images currently in the latest (and watched) flight to the frontend
+                            # Sends all images currently in the latest (and watched) flight to the frontend
                             for i in client.list_objects(bucket_name=activeFlight):
-                                await manager.sendImgData(activeFlight,i.object_name)
-                        except(IndexError):
-                            #Not entirely sure why this happens, but no problems are caused by this exception block
+                                await manager.sendImgData(activeFlight, i.object_name)
+                        except IndexError:
+                            # Not entirely sure why this happens, but no problems are caused by this exception block
                             pass
                     else:
-                        #Flow of control goes here if the user is looking at past flights
-                        watchingFlight = data['flight_id']
+                        # Flow of control goes here if the user is looking at past flights
+                        watchingFlight = data["flight_id"]
                         for i in client.list_objects(bucket_name=watchingFlight):
-                            await manager.sendImgData(watchingFlight,i.object_name)
+                            await manager.sendImgData(watchingFlight, i.object_name)
             print("awaiting next message")
-    except(RuntimeError):
-        return("WS Disconnected")
-    except(WebSocketDisconnect):
-        return ("WS Disconnected")
-            
-        
+    except RuntimeError:
+        return "WS Disconnected"
+    except WebSocketDisconnect:
+        return "WS Disconnected"
 
-@app.get('/checkBucket/')
+
+@app.get("/checkBucket/")
 def checkBucket() -> str:
-    '''
+    """
     Checks that the bucket that we are wanting to use exists, if it does not exist, it will create a bucket, will print to console the bucket being used.
     Acts both as a function called within the program before any additions are made to the bucket and as a method for the user the check the bucket they are using
-    '''
+    """
     exists = client.bucket_exists(str(activeFlight))
 
     if not exists:
         client.make_bucket(bucket_name=activeFlight)
-    print("Using bucket: "+activeFlight)
-    return("Using bucket: "+activeFlight)
-    
+    print("Using bucket: " + activeFlight)
+    return "Using bucket: " + activeFlight
 
-    
 
-@app.post('/shimmer/')
-async def create_upload_file(file: UploadFile = File(...), metadata: Optional[str] = Form(None))  -> str:
-    '''
+@app.post("/shimmer/")
+async def create_upload_file(
+    file: UploadFile = File(...), metadata: Optional[str] = Form(None)
+) -> str:
+    """
     Recieves image from post request, and stores it in the database and sends it to the frontend
-    '''
+    """
     global firstSend
     global activeFlight
     global watchingFlight
@@ -179,99 +197,110 @@ async def create_upload_file(file: UploadFile = File(...), metadata: Optional[st
         activeFlight = str(datetime.datetime.now().strftime("%Y.%m.%d-%H.%M.%S"))
     else:
         activeFlight = getLatestBucket()
-        
-    if (watchingFlight == ""):
+
+    if watchingFlight == "":
         watchingFlight = activeFlight
-        
+
     firstSend = False
     checkBucket()
-    #Delete all files in temp_images
-    ''' Removed from test flight 1 
+    # Delete all files in temp_images
+    """ Removed from test flight 1 
     for old_file in os.listdir(temp_directory):
         path = os.path.join(temp_directory,old_file)
         os.remove(path)
-    '''
-    #Turn the coroutine into an image
+    """
+
+    # Turn the coroutine into an image - We have to use client.fput because the type UploadFile is not specifically an image, so using fput makes handling the image more efficient
     im = Image.open(io.BytesIO(await file.read()))
-    path = os.path.join(temp_directory,str(file.filename))
+    path = os.path.join(temp_directory, str(file.filename))
     print(path)
     im.save(path)
-    #Set the data to send to frontend
-    #Upload image to database
-    #Checks if the image is already in the database
+    # Set the data to send to frontend
+    # Upload image to database
+    # Checks if the image is already in the database
     try:
-        client.stat_object(bucket_name=activeFlight,object_name=file.filename)
+        client.stat_object(bucket_name=activeFlight, object_name=file.filename)
         im.close()
-        return("File Already Exists")
-    #Runs if the image doesn't already exist
-    except(S3Error):
-        #Verify Metadata
+        return "File Already Exists"
+    # Runs if the image doesn't already exist
+    except S3Error:
+        # Verify Metadata
         if metadata is None:
             print("Invalid Metadata Supplied - Defaulting to width and height")
-            metadata = {"width":str(im.width), "height":str(im.height)}
+            metadata = {"width": str(im.width), "height": str(im.height)}
         else:
             metadata = json.loads(metadata)
             print("Valid Metadata Supplied: {0}".format(metadata))
-            
+
         new_tag = Tags(for_object=True)
-        new_tag["process"]="True"
-        
-        client.fput_object(bucket_name=activeFlight,object_name=file.filename,file_path=str(path),tags=new_tag,metadata=metadata)
-        im.close()
-        
+        new_tag["process"] = "True"
+
+        client.fput_object(
+            bucket_name=activeFlight,
+            object_name=file.filename,
+            file_path=str(path),
+            tags=new_tag,
+            metadata=metadata,
+        )
+
         if watchingFlight == activeFlight:
-            #Send data to frontend, notifying that an image has been added
+            # Send data to frontend, notifying that an image has been added
             try:
-                await manager.sendImgData(flight_id=activeFlight,img_id=file.filename)
+                await manager.sendImgData(flight_id=activeFlight, img_id=file.filename)
             except HTTPException:
                 print("Frontend Connection Inactive")
-            
+
         try:
-            requests.get(adlcAddress,params=im)
+            requests.get(adlcAddress, params=im)
         except:
             print("ADLC not connected")
         print("---------------------------------------------------")
-        return('success')
+
+        im.close()
+        return "success"
+
 
 @app.get("/get_img/{img_id}")
-async def getImg(img_id:str) -> Response:
-    '''
+async def getImg(img_id: str) -> Response:
+    """
     Endpoint that displays the specified img
-    '''
-    img = client.get_object(bucket_name=watchingFlight,object_name=img_id).data
-    return Response(content=img,media_type="image")
+    """
+    img = client.get_object(bucket_name=watchingFlight, object_name=img_id).data
+    return Response(content=img, media_type="image")
+
 
 @app.get("/get_flights")
 async def getFlights() -> list:
-    '''
+    """
     Endpoint to get a list of all created buckets
-    '''
+    """
     flights = []
     for flight in client.list_buckets():
         flights.append(flight.name)
-    return (flights)
+    return flights
 
-@app.get('/list/{flight_id}')
-async def listImages(flight_id:str) -> list:
-    '''
+
+@app.get("/list/{flight_id}")
+async def listImages(flight_id: str) -> list:
+    """
     Lists all files in the active bucket
-    '''
+    """
     checkBucket()
     imgs = list()
-    for i in client.list_objects(bucket_name=flight_id,recursive=True):
+    for i in client.list_objects(bucket_name=flight_id, recursive=True):
         imgs.append(i.object_name)
     imgs.sort()
     return imgs
 
-@app.get('/get_img_metadata/{img}')
-async def getImgMetadata(img:str) -> dict:
-    '''
-    Endpoint to get the metadata of an image in the flight that the frontend is watching 
-    '''
+
+@app.get("/get_img_metadata/{img}")
+async def getImgMetadata(img: str) -> dict:
+    """
+    Endpoint to get the metadata of an image in the flight that the frontend is watching
+    """
     img_info = client.stat_object(bucket_name=watchingFlight, object_name=img).metadata
     metadata = {}
     for field in img_info:
-        if ("x-amz" in field):
+        if "x-amz" in field:
             metadata[field] = img_info[field]
     return metadata
-
